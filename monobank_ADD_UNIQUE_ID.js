fd20b3d87b06cbc30e5ce6e23910766f1c0956ce
44-5-53-6-k <80600884+44-5-53-6-k@users.noreply.github.com>
2023-01-22 18:07:50 +0100

    Create monobank_ADD_UNIQUE_ID.js

---
+++ monobank_ADD_UNIQUE_ID.js
@@ -0,0 +1,71 @@
+
+
+const { Client } = require('@notionhq/client');
+const notion = new Client({ auth: "secret_fZS8vj1Xcoce304v0AvvGjPnKQGKxNn7dCh3ukVeZyf" });
+
+const queryDatabase = require('./../helpers/findAccount')
+
+const ENTRIES_DATABASE_ID = "paste it here";
+const accountDatabaseID = "paste it here";
+
+/**
+ * An HTTP endpoint that acts as a webhook for HTTP(S) request event
+ * @returns {any} result
+ */
+module.exports = async (context) => {
+
+  if (context.http.method === 'POST') {
+    console.log(context.http.body)
+    let jsonData = JSON.parse(context.http.body);
+    
+    (async () => {
+      const account = await queryDatabase(accountDatabaseID, jsonData.data.account);
+    
+      const response = await notion.pages.create({
+        "parent": {
+          "type": "database_id",
+          "database_id": ENTRIES_DATABASE_ID
+        },
+        "properties": {
+          "Account": {
+            "relation": [
+              {
+                "id": account
+              }
+            ]
+          },
+          "Date": {
+            "date": {
+              "start": new Date().toISOString()
+            }
+          },
+          "Expense": {
+            "title": [
+              {
+                "text": {
+                  "content": jsonData.data.statementItem.description
+                }
+              }
+            ]
+          },
+          "Amount": {
+            "number": jsonData.data.statementItem.amount/100
+          },
+          
+        }
+      });
+      console.log(response);
+    })();
+    
+    
+    return {
+      statusCode: 200,
+    };
+  } else {
+    throw new Error('We only respond to POST!');
+  }
+
+};
+
+
+
